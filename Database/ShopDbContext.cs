0bbbb813e8a102e3e6e88d68780efe72bd496feb
Максим <jakdman2003@gmail.com>
2023-11-11 09:24:39 +0300

    feat(Database): Установил библиотеки, создал контекст базы данных

---
+++ Database/ShopDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace shop_backend.Database
+{
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options) { }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using shop_backend.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,8 +15,12 @@ namespace shop_backend
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup (IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors(options =>
@@ -24,10 +31,16 @@ namespace shop_backend
                                       .AllowAnyHeader()
                                       .AllowCredentials());
             });
+
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ShopDbContext>(options =>
+            {
+                options.UseMySql(connection, ServerVersion.AutoDetect(connection));
+            });
+
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseCors("AllowOrigin");
